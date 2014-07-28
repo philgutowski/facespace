e624f7037b9e4f950821a3a87545a29ae6418334
katherineabuhadal <katherine8@verizon.net>
2014-07-28 11:35:00 -0400

    Change email auth to username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,6 +22,6 @@ class SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:session).permit(:email, :password)
+    params.require(:session).permit(:name, :password)
   end
 end
