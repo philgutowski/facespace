e35e0a63b7609040beab431c254dc606d2c5abcf
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-30 11:52:36 -0400

    Make header fixed

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -1,13 +1,11 @@
 $(function(){
   var pusher = new Pusher(window.PUSHER_KEY);
   var channel = pusher.subscribe('chat_channel');
+  $(".chatroom").scrollTop(99000);
 
   channel.bind('chat-event', function(data) {
-    $(".messages").append(data.message);
-  });
-
-  channel.bind('online-users-event', function(data) {
-    $("#online_users").html(data.message);
+    $("#messages").append(data.message);
+    $(".chatroom").scrollTop(99000);
   });
 
   $("#new_message").submit(function() {
@@ -15,7 +13,11 @@ $(function(){
       $(this).prop("action"),
       $(this).serialize()
     ).done(function(data){
+<<<<<<< HEAD
       $("#message_body, #message_image").val("");
+=======
+      $("#message_body").val("");
+>>>>>>> Make header fixed
     });
     return false;
   });
