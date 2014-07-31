072b478fc63c471408f47dd756310b363fffb082
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-31 10:55:42 -0400

    Install fog

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -7,5 +7,5 @@ CarrierWave.configure do |config|
   }
   config.fog_directory  = 'facespace-images'                     # required
   config.fog_public     = true                                   # optional, defaults to true
-  # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
