947183728a04ae9953a03120493b8627a4149441
Sinclair <sbk64@cornell.edu>
2014-07-31 10:10:12 -0400

    place the most recent post in bottom

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
   def show
     @message = Message.new
-    @messages = Message.order(created_at: :asc).limit(50)
+    @messages = Message.order(created_at: :asc).limit(50).sort_by(&:created_at)
     @online_users = User.recently_online
   end
 end
