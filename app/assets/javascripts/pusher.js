246b26ae6253c10bc8354c4fac7a75b7c9df2f88
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-31 16:20:12 -0400

    Improve indentation

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -1,14 +1,14 @@
+function scroll_to_bottom() {
+  $('html, body').scrollTop( $(document).height() );
+}
+
 $(function(){
-  function scroll_to_bottom() {
-    $('html, body').scrollTop( $(document).height() );
-  }
   var pusher = new Pusher(window.PUSHER_KEY);
   var channel = pusher.subscribe('chat_channel');
 
   scroll_to_bottom();
   channel.bind('chat-event', function(data) {
     $(".messages").append(data.message);
-
   });
 
   channel.bind('online-users-event', function(data) {
@@ -19,7 +19,7 @@ $(function(){
     $.post(
       $(this).prop("action"),
       $(this).serialize()
-      ).done(function(data){
+    ).done(function(data){
         $("#message_body, #message_image").val("");
         scroll_to_bottom();
       });
