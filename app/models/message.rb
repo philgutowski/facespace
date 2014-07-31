c5a2832e32c1e85b269ba7c16f2f1da249af212c
katherineabuhadal <katherine8@verizon.net>
2014-07-31 11:22:45 -0400

    best of

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,20 @@ class Message < ActiveRecord::Base
 
   belongs_to :user
   has_many :votes, dependent: :destroy
+
   validates :user, presence: true
   validates :body, presence: true
   validates :image, presence: true
+
+  def score
+    votes.sum("value")
+  end
+
+  def self.top_five
+    joins(:votes)
+      .select("messages.*, SUM(votes.value) AS score")
+      .group("messages.id")
+      .order("score DESC")
+      .limit(5)
+  end
 end
